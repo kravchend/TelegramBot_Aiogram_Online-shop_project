1a06379f1b5f1e5d09a828e3c067ba87d634f17b
kravchend <kravchend@gmail.com>
2025-05-27 01:51:01 +0200

    Доработана функция заполнения полей от пользователя

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import ReplyKeyboardMarkup, WebAppInfo, KeyboardButton
+from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types.web_app_info import WebAppInfo
+import json
 import asyncio
 from config import TOKEN_KEY
 
@@ -8,22 +10,33 @@ from config import TOKEN_KEY
 async def start(message: types.Message):
     markup = ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="Открыть веб страницу", web_app=WebAppInfo(url="https://kravchend.github.io/TelegramBot_Aiogram_Online-shop_project/"))]
+            [
+                KeyboardButton(
+                    text="🚀 Открыть веб страницу",
+                    web_app=WebAppInfo(url="https://kravchend.github.io/TelegramBot_Aiogram_Online-shop_project/")
+                )
+            ]
         ],
-        resize_keyboard=True
+        resize_keyboard=True  # чтобы кнопка подходила под размер экрана
     )
     await message.answer(f"Привет, {message.from_user.full_name}!",
                          reply_markup=markup)
 
 
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f"Name: {res['name']}. Email: {res['email']}. Phone: {res['phone']}.")
+
+
 async def main():
     bot = Bot(token=TOKEN_KEY)
     dp = Dispatcher()
-
     # Регистрируем функцию /start
     dp.message.register(start, Command('start'))
-
+    # Регистрируем функцию web_app
+    dp.message.register(web_app, lambda m: m.web_app_data is not None)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
