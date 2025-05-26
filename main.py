0276a9049ec83f62a4f5aa3780fb96eecbc03b89
kravchend <kravchend@gmail.com>
2025-05-26 19:04:45 +0200

    Изменения в коде файла main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram.types import ReplyKeyboardMarkup, WebAppInfo, KeyboardButton
 import asyncio
 from config import TOKEN_KEY
 
+
 async def start(message: types.Message):
     markup = ReplyKeyboardMarkup(
         keyboard=[
@@ -14,18 +15,13 @@ async def start(message: types.Message):
     await message.answer(f"Привет, {message.from_user.full_name}!",
                          reply_markup=markup)
 
-async def info(message: types.Message):
-    await message.answer(
-        "Это бот, который будет отвечать на ваши запросы",
-    )
 
 async def main():
     bot = Bot(token=TOKEN_KEY)
     dp = Dispatcher()
 
-    # Регистрируем хендлер с помощью метода message
+    # Регистрируем функцию /start
     dp.message.register(start, Command('start'))
-    dp.message.register(info, Command("info"))
 
     await dp.start_polling(bot)
 
