97aa941e13133815de781274b95b68bdff511e1b
kravchend <kravchend@gmail.com>
2025-05-29 16:58:15 +0200

    Добален модуль python-dotenv и обновлен requirements.txt

---
+++ app.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer(f'Здравствуйте {message.from_user.full_name}, мы приветствуем Вас в нашей Онлайн-пиццерии!')
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+    await message.reply(message.text)
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
