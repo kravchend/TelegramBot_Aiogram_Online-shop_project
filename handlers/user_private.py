16a3e022e0dc677786fe8f29aa551949767d27c1
kravchend <kravchend@gmail.com>
2025-05-30 13:13:54 +0200

    Добвлена группа и код для удаленияя сообщений содержащих запрещенные слова

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,21 +1,28 @@
 from aiogram import F, types, Router
 from aiogram.filters import CommandStart, Command, or_f
-from filters.chat_type import ChatTypeFilter
+from filters.chat_types import ChatTypeFilter
 
 user_private_router = Router()
-
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer(f'Здравствуйте {message.from_user.full_name}, я виртуальный помощник!')
+    await message.answer(f"Привет {message.from_user.full_name}, я виртуальный помощник")
 
 
+# @user_private_router.message(F.text.lower() == "меню")
 @user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
 async def menu_cmd(message: types.Message):
     await message.answer("Вот меню:")
 
 
 @user_private_router.message(F.text.lower() == "о нас")
+@user_private_router.message(Command("about"))
+async def about_cmd(message: types.Message):
+    await message.answer("О нас:")
+
+
+@user_private_router.message(F.text.lower() == "варианты оплаты")
 @user_private_router.message(Command("payment"))
 async def payment_cmd(message: types.Message):
     await message.answer("Варианты оплаты:")
@@ -25,4 +32,3 @@ async def payment_cmd(message: types.Message):
 @user_private_router.message(Command("shipping"))
 async def menu_cmd(message: types.Message):
     await message.answer("Варианты доставки:")
-
