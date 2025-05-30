9dfb9cbf256e52d3840ed60d00d4bb69c82576e1
kravchend <kravchend@gmail.com>
2025-05-30 16:52:11 +0200

    Добавлены кнопки

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 from dotenv import find_dotenv, load_dotenv
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 load_dotenv(find_dotenv())
 
@@ -12,7 +14,10 @@ from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ['message, edited_message']
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(
+    token=os.getenv('TOKEN'),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
