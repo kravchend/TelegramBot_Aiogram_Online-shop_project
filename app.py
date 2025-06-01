75ba50ddbe92a61716d550ec76a5bd3afe31496c
kravchend <kravchend@gmail.com>
2025-06-01 16:10:01 +0200

    Добавлен каталог, корзина и функции меню

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from handlers.admin_private import admin_router
 
-from common.bot_cmds_list import private
+# from common.bot_cmds_list import private
 
 
 # ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
@@ -26,6 +26,7 @@ bot = Bot(
     token=os.getenv('TOKEN'),
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
+
 bot.my_admins_list = []
 
 dp = Dispatcher()
@@ -37,9 +38,7 @@ dp.include_router(admin_router)
 
 async def on_startup(bot):
 
-    run_param = False
-    if run_param:
-        await drop_db()
+    # await drop_db()
 
     await create_db()
 
@@ -56,7 +55,7 @@ async def main():
 
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
-    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
+    # await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
